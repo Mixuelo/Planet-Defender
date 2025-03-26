3410b207b00b19cfebc074ff017c4db8f5ea8aac
Mixuelo <miguelalvito@hotmail.com>
2025-03-26 15:17:31 +0000

    Operações para Collisores para inicializaçao e translaçao
    Falta testar novas operaçoes para pontos, colisores em circulo
    Falta implementar estas operaçoes para colisores em poligono

--- src/Collider.java
+++ src/Collider.java
@@ -5,12 +5,32 @@
 public abstract class Collider implements ICollider
 {
     protected Point centroid;
+    protected double scale;
+    protected double angle;
 
     /** Devolve o centroide do Collider */
     public Point centroid()
     {
         return this.centroid;
     }
+
+    //TODO: DOCUMENTAR
+    public abstract void scale(double dScale);
+
+    //TODO: DOCUMENTAR
+    public abstract void move(Point dPos);
+
+    //TODO: DOCUMENTAR
+    public abstract void rotate(double dAngle);
+
+    //TODO: DOCUMENTAR
+    public abstract void setScale(double scale);
+
+    //TODO: DOCUMENTAR
+    public abstract void setCentroid(Point pos);
+
+    //TODO: DOCUMENTAR
+    public abstract void setAngle(double angle);
 
     /** Devolve uma representação em String do Collider */
     @Override
--- src/ColliderCircle.java
+++ src/ColliderCircle.java
@@ -6,6 +6,7 @@
 public class ColliderCircle extends Collider 
 {
     private double radius;
+    private double scale;
     
     /** Construtor do Collider em circulo
         @param center   o centro do circulo, em Point
@@ -16,6 +17,14 @@ public class ColliderCircle extends Collider
         checkRadius(radius);
         this.centroid = center.clone();
         this.radius = radius;
+        this.scale = 1;
+    }
+
+    //TODO: DOCUMENTAR e TESTAR
+    public ColliderCircle(Transform trans, Point center, double radius)
+    {
+        this(center, radius);
+        this.scale(trans.scale());
     }
 
     /** Verificar se o raio é maior que 0, caso nao for dá-se uma exceção
@@ -30,6 +39,47 @@ public class ColliderCircle extends Collider
     public double radius()
     {
         return this.radius;
+    }
+
+    //TODO: DOCUMENTAR e TESTAR
+    //@pre  s > 0
+    public void scale(double dScale)
+    {
+        this.radius *= dScale;
+        this.scale *= dScale;
+    }
+
+    //TODO: DOCUMENTAR e TESTAR
+    public void move(Point dPos)
+    {
+        this.centroid.addThis(dPos);
+    }
+
+    //TODO: DOCUMENTAR
+    public void rotate(double dAngle)
+    {
+        this.angle += dAngle;
+    }
+
+    //TODO: DOCUMENTAR e TESTAR
+    //@pre  s > 0
+    public void setScale(double s)
+    {
+        double d = s / this.scale;
+        this.scale(d);
+    }
+
+    //TODO: DOCUMENTAR e TESTAR
+    public void setCentroid(Point pos)
+    {
+        Point diff = pos.subNew(this.centroid);
+        this.centroid.addThis(diff);
+    }
+
+    //TODO: DOCUMENTAR e TESTAR
+    public void setAngle(double a)
+    {
+        this.angle = a;
     }
 
     /** Devolve uma representação em String do Collider, no formato: "<centroid> <radius>" */
